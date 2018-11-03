cb712e2bb5957e6f6f7425d875a99d22928be6de
hoooori <hideyasu.horiuchi@gmail.com>
2018-11-03 15:59:27 +0900

    feat:implemented recipe search and render function

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,22 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+
+const state = {};
+
+const controlSearch = async () => {
+  const query = searchView.getInput(); // 1. viewからqueryを取得
+  if(query) {
+    state.search = new Search(query); // 2. searchオブジェクトを作成し、stateに保持
+    searchView.clearInput(); // 3. 次の検索の為に入力フォームをリセット
+    searchView.clearResults(); // 4. 検索結果を空にする
+    await state.search.getResults(); // 5. レシピを検索
+    searchView.renderResults(state.search.result); // 5. 検索結果をviewに表示
+  }
+}
+
+// 検索ボタンに検索処理を実装(ボタンクリック駆動)
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+})
